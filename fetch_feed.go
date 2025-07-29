1ffc2f56e59f5d1aaf237cc5bcb2aabe05f9a0f9
Marco Castellanos <mcbk51.marcopoulut@gmail.com>
2025-07-29 09:37:41 -0700

    rss setup

---
+++ fetch_feed.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+)
+
+type RSSFeed struct {
+	Channel struct {
+		Title       string    `xml:"title"`
+		Link        string    `xml:"link"`
+		Description string    `xml:"description"`
+		Items       []RSSItem `xml:"item"`
+	} `xml:"channel"`
+}
+
+type RSSItem struct {
+	Title       string `xml:"title"`
+	Link        string `xml:"link"`
+	Description string `xml:"description"`
+	PubDate     string `xml:"pubDate"`
+}
+
+func fetchFeed(ctx context.Context, feedURL string) (*RSSFeed, error) {
+	req, err := http.NewRequestWithContext(ctx, "GET", feedURL, nil)	
+	if err != nil {
+		return nil, fmt.Errorf("failed to create request: %w", err)
+	}
+
+  req.Header.Set("User-Agent", "gator") 
+
+  client := &http.Client{}
+	resp, err := client.Do(req)
+  if err != nil {
+		return nil, fmt.Errorf("failed to fetch data: %w", err)
+  }
+	defer resp.Body.Close()
+
+	if resp.StatusCode >= 400 {
+		return nil, fmt.Errorf("bad status code: %d", resp.StatusCode) 
+	}
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, fmt.Errorf("failed to read response: %w", err)
+	}
+
+	var feed RSSFeed
+	err = xml.Unmarshal(body, &feed)
+	if err != nil {
+		return nil, fmt.Errorf("failed to parse feed: %w", err)
+	}
+
+	feed.Channel.Title = html.UnescapeString(feed.Channel.Title)
+	feed.Channel.Description = html.UnescapeString(feed.Channel.Description)
+
+	for i := range feed.Channel.Items {
+		feed.Channel.Items[i].Title = html.UnescapeString(feed.Channel.Items[i].Title)
+		feed.Channel.Items[i].Description = html.UnescapeString(feed.Channel.Items[i].Description)
+	}
+	return &feed, nil
+
+}
--- handler_user.go
+++ handler_user.go
@@ -64,9 +64,9 @@ func handlerListUsers(s *state, cmd command) error {
 
 	for _, user := range users {
 		if user.Name == s.cfg.CurrentUserName {
-			fmt.Printf("* %s (current)\n", user.Name)
+			fmt.Printf("* %v (current)\n", user.Name)
 		} else {
-			fmt.Printf("* %s\n", user.Name)
+			fmt.Printf("* %v\n", user.Name)
 		}
 	}
 
