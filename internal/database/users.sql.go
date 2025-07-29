04c0822aa10a16da7ccde5879e048f9fcf324ae5
Marco Castellanos <mcbk51.marcopoulut@gmail.com>
2025-07-29 14:04:57 -0700

    list feeds

--- internal/database/users.sql.go
+++ internal/database/users.sql.go
@@ -72,12 +72,12 @@ func (q *Queries) GetUser(ctx context.Context, name string) (User, error) {
 	return i, err
 }
 
-const getUserByID = `-- name: GetUserByID :one
+const getUserById = `-- name: GetUserById :one
 SELECT id, created_at, updated_at, name FROM users WHERE id = $1
 `
 
-func (q *Queries) GetUserByID(ctx context.Context, id uuid.UUID) (User, error) {
-	row := q.db.QueryRowContext(ctx, getUserByID, id)
+func (q *Queries) GetUserById(ctx context.Context, id uuid.UUID) (User, error) {
+	row := q.db.QueryRowContext(ctx, getUserById, id)
 	var i User
 	err := row.Scan(
 		&i.ID,
--- sql/queries/users.sql
+++ sql/queries/users.sql
@@ -17,5 +17,5 @@ DELETE FROM users;
 -- name: GetUsers :many
 SELECT * FROM users;
 
--- name: GetUserByID :one
+-- name: GetUserById :one
 SELECT * FROM users WHERE id = $1;
