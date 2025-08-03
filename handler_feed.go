a4519efdabf00d2adc81ad6dd830707b1931809b
Marco Castellanos <marcopoulut@gmail.com>
2025-08-03 11:47:36 -0700

    post finished

--- handler_feed.go
+++ handler_feed.go
@@ -81,5 +81,6 @@ func printFeed(feed database.Feed, user database.User) {
 	fmt.Printf("* Name:          %s\n", feed.Name)
 	fmt.Printf("* URL:           %s\n", feed.Url)
 	fmt.Printf("* User:          %s\n", user.Name)
+	fmt.Printf("* LastFetchedAt  %v\n", feed.LastFetchedAt.Time)
 }
 
